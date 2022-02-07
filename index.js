3bad38634705fc3c53d60a5187a619d61ecab818
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-02-07 16:38:32 -0500

    commit tree d873531f90a6a9cac94f4549b73990443f7e2a6f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(users, name){
+    const result = users.filter(user => user.toUpperCase() === name.toUpperCase() );
+    return result;
+}
+
+
+function fuzzyMatch(users, name){
+    const result = users.filter(user => user.charAt(0) === name.charAt(0) );
+    return result
+}
+ 
+function matchName(array, name){
+    const result = array.filter(element => element.name === name);
+    return result;
+}
